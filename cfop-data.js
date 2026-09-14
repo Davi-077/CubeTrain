cc3e8464846c1e83c47c805f16b2af43d252425c
Davi-077 <davisouzasanchez@gmail.com>
2026-09-14 18:23:21 -0300

    Add files via upload

---
+++ cfop-data.js
@@ -0,0 +1,722 @@
+window.CFOP_DATA = {
+  "F2L": [
+    {
+      "id": "F2L 1",
+      "name": "Caso F2L 1",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_1"
+    },
+    {
+      "id": "F2L 2",
+      "name": "Caso F2L 2",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_2"
+    },
+    {
+      "id": "F2L 3",
+      "name": "Caso F2L 3",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_3"
+    },
+    {
+      "id": "F2L 4",
+      "name": "Caso F2L 4",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_4"
+    },
+    {
+      "id": "F2L 5",
+      "name": "Caso F2L 5",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_5"
+    },
+    {
+      "id": "F2L 6",
+      "name": "Caso F2L 6",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_6"
+    },
+    {
+      "id": "F2L 7",
+      "name": "Caso F2L 7",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_7"
+    },
+    {
+      "id": "F2L 8",
+      "name": "Caso F2L 8",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_8"
+    },
+    {
+      "id": "F2L 9",
+      "name": "Caso F2L 9",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_9"
+    },
+    {
+      "id": "F2L 10",
+      "name": "Caso F2L 10",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_10"
+    },
+    {
+      "id": "F2L 11",
+      "name": "Caso F2L 11",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_11"
+    },
+    {
+      "id": "F2L 12",
+      "name": "Caso F2L 12",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_12"
+    },
+    {
+      "id": "F2L 13",
+      "name": "Caso F2L 13",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_13"
+    },
+    {
+      "id": "F2L 14",
+      "name": "Caso F2L 14",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_14"
+    },
+    {
+      "id": "F2L 15",
+      "name": "Caso F2L 15",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_15"
+    },
+    {
+      "id": "F2L 16",
+      "name": "Caso F2L 16",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_16"
+    },
+    {
+      "id": "F2L 17",
+      "name": "Caso F2L 17",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_17"
+    },
+    {
+      "id": "F2L 18",
+      "name": "Caso F2L 18",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_18"
+    },
+    {
+      "id": "F2L 19",
+      "name": "Caso F2L 19",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_19"
+    },
+    {
+      "id": "F2L 20",
+      "name": "Caso F2L 20",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_20"
+    },
+    {
+      "id": "F2L 21",
+      "name": "Caso F2L 21",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_21"
+    },
+    {
+      "id": "F2L 22",
+      "name": "Caso F2L 22",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_22"
+    },
+    {
+      "id": "F2L 23",
+      "name": "Caso F2L 23",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_23"
+    },
+    {
+      "id": "F2L 24",
+      "name": "Caso F2L 24",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_24"
+    },
+    {
+      "id": "F2L 25",
+      "name": "Caso F2L 25",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_25"
+    },
+    {
+      "id": "F2L 26",
+      "name": "Caso F2L 26",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_26"
+    },
+    {
+      "id": "F2L 27",
+      "name": "Caso F2L 27",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_27"
+    },
+    {
+      "id": "F2L 28",
+      "name": "Caso F2L 28",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_28"
+    },
+    {
+      "id": "F2L 29",
+      "name": "Caso F2L 29",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_29"
+    },
+    {
+      "id": "F2L 30",
+      "name": "Caso F2L 30",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_30"
+    },
+    {
+      "id": "F2L 31",
+      "name": "Caso F2L 31",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_31"
+    },
+    {
+      "id": "F2L 32",
+      "name": "Caso F2L 32",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_32"
+    },
+    {
+      "id": "F2L 33",
+      "name": "Caso F2L 33",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_33"
+    },
+    {
+      "id": "F2L 34",
+      "name": "Caso F2L 34",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_34"
+    },
+    {
+      "id": "F2L 35",
+      "name": "Caso F2L 35",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_35"
+    },
+    {
+      "id": "F2L 36",
+      "name": "Caso F2L 36",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_36"
+    },
+    {
+      "id": "F2L 37",
+      "name": "Caso F2L 37",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_37"
+    },
+    {
+      "id": "F2L 38",
+      "name": "Caso F2L 38",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_38"
+    },
+    {
+      "id": "F2L 39",
+      "name": "Caso F2L 39",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_39"
+    },
+    {
+      "id": "F2L 40",
+      "name": "Caso F2L 40",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_40"
+    },
+    {
+      "id": "F2L 41",
+      "name": "Caso F2L 41",
+      "group": "F2L",
+      "ref": "https://www.speedcubedb.com/a/3x3/F2L/F2L_41"
+    }
+  ],
+  "OLL": [
+    {
+      "id": "OLL 1",
+      "name": "OLL 1",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_1"
+    },
+    {
+      "id": "OLL 2",
+      "name": "OLL 2",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_2"
+    },
+    {
+      "id": "OLL 3",
+      "name": "OLL 3",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_3"
+    },
+    {
+      "id": "OLL 4",
+      "name": "OLL 4",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_4"
+    },
+    {
+      "id": "OLL 5",
+      "name": "OLL 5",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_5"
+    },
+    {
+      "id": "OLL 6",
+      "name": "OLL 6",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_6"
+    },
+    {
+      "id": "OLL 7",
+      "name": "OLL 7",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_7"
+    },
+    {
+      "id": "OLL 8",
+      "name": "OLL 8",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_8"
+    },
+    {
+      "id": "OLL 9",
+      "name": "OLL 9",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_9"
+    },
+    {
+      "id": "OLL 10",
+      "name": "OLL 10",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_10"
+    },
+    {
+      "id": "OLL 11",
+      "name": "OLL 11",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_11"
+    },
+    {
+      "id": "OLL 12",
+      "name": "OLL 12",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_12"
+    },
+    {
+      "id": "OLL 13",
+      "name": "OLL 13",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_13"
+    },
+    {
+      "id": "OLL 14",
+      "name": "OLL 14",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_14"
+    },
+    {
+      "id": "OLL 15",
+      "name": "OLL 15",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_15"
+    },
+    {
+      "id": "OLL 16",
+      "name": "OLL 16",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_16"
+    },
+    {
+      "id": "OLL 17",
+      "name": "OLL 17",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_17"
+    },
+    {
+      "id": "OLL 18",
+      "name": "OLL 18",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_18"
+    },
+    {
+      "id": "OLL 19",
+      "name": "OLL 19",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_19"
+    },
+    {
+      "id": "OLL 20",
+      "name": "OLL 20",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_20"
+    },
+    {
+      "id": "OLL 21",
+      "name": "OLL 21",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_21"
+    },
+    {
+      "id": "OLL 22",
+      "name": "OLL 22",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_22"
+    },
+    {
+      "id": "OLL 23",
+      "name": "OLL 23",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_23"
+    },
+    {
+      "id": "OLL 24",
+      "name": "OLL 24",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_24"
+    },
+    {
+      "id": "OLL 25",
+      "name": "OLL 25",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_25"
+    },
+    {
+      "id": "OLL 26",
+      "name": "OLL 26",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_26"
+    },
+    {
+      "id": "OLL 27",
+      "name": "OLL 27",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_27"
+    },
+    {
+      "id": "OLL 28",
+      "name": "OLL 28",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_28"
+    },
+    {
+      "id": "OLL 29",
+      "name": "OLL 29",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_29"
+    },
+    {
+      "id": "OLL 30",
+      "name": "OLL 30",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_30"
+    },
+    {
+      "id": "OLL 31",
+      "name": "OLL 31",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_31"
+    },
+    {
+      "id": "OLL 32",
+      "name": "OLL 32",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_32"
+    },
+    {
+      "id": "OLL 33",
+      "name": "OLL 33",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_33"
+    },
+    {
+      "id": "OLL 34",
+      "name": "OLL 34",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_34"
+    },
+    {
+      "id": "OLL 35",
+      "name": "OLL 35",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_35"
+    },
+    {
+      "id": "OLL 36",
+      "name": "OLL 36",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_36"
+    },
+    {
+      "id": "OLL 37",
+      "name": "OLL 37",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_37"
+    },
+    {
+      "id": "OLL 38",
+      "name": "OLL 38",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_38"
+    },
+    {
+      "id": "OLL 39",
+      "name": "OLL 39",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_39"
+    },
+    {
+      "id": "OLL 40",
+      "name": "OLL 40",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_40"
+    },
+    {
+      "id": "OLL 41",
+      "name": "OLL 41",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_41"
+    },
+    {
+      "id": "OLL 42",
+      "name": "OLL 42",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_42"
+    },
+    {
+      "id": "OLL 43",
+      "name": "OLL 43",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_43"
+    },
+    {
+      "id": "OLL 44",
+      "name": "OLL 44",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_44"
+    },
+    {
+      "id": "OLL 45",
+      "name": "OLL 45",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_45"
+    },
+    {
+      "id": "OLL 46",
+      "name": "OLL 46",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_46"
+    },
+    {
+      "id": "OLL 47",
+      "name": "OLL 47",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_47"
+    },
+    {
+      "id": "OLL 48",
+      "name": "OLL 48",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_48"
+    },
+    {
+      "id": "OLL 49",
+      "name": "OLL 49",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_49"
+    },
+    {
+      "id": "OLL 50",
+      "name": "OLL 50",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_50"
+    },
+    {
+      "id": "OLL 51",
+      "name": "OLL 51",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_51"
+    },
+    {
+      "id": "OLL 52",
+      "name": "OLL 52",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_52"
+    },
+    {
+      "id": "OLL 53",
+      "name": "OLL 53",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_53"
+    },
+    {
+      "id": "OLL 54",
+      "name": "OLL 54",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_54"
+    },
+    {
+      "id": "OLL 55",
+      "name": "OLL 55",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_55"
+    },
+    {
+      "id": "OLL 56",
+      "name": "OLL 56",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_56"
+    },
+    {
+      "id": "OLL 57",
+      "name": "OLL 57",
+      "group": "OLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/OLL/OLL_57"
+    }
+  ],
+  "PLL": [
+    {
+      "id": "PLL Aa",
+      "name": "PLL Aa",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Aa"
+    },
+    {
+      "id": "PLL Ab",
+      "name": "PLL Ab",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Ab"
+    },
+    {
+      "id": "PLL E",
+      "name": "PLL E",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/E"
+    },
+    {
+      "id": "PLL F",
+      "name": "PLL F",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/F"
+    },
+    {
+      "id": "PLL Ga",
+      "name": "PLL Ga",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Ga"
+    },
+    {
+      "id": "PLL Gb",
+      "name": "PLL Gb",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Gb"
+    },
+    {
+      "id": "PLL Gc",
+      "name": "PLL Gc",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Gc"
+    },
+    {
+      "id": "PLL Gd",
+      "name": "PLL Gd",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Gd"
+    },
+    {
+      "id": "PLL H",
+      "name": "PLL H",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/H"
+    },
+    {
+      "id": "PLL Ja",
+      "name": "PLL Ja",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Ja"
+    },
+    {
+      "id": "PLL Jb",
+      "name": "PLL Jb",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Jb"
+    },
+    {
+      "id": "PLL Na",
+      "name": "PLL Na",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Na"
+    },
+    {
+      "id": "PLL Nb",
+      "name": "PLL Nb",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Nb"
+    },
+    {
+      "id": "PLL Ra",
+      "name": "PLL Ra",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Ra"
+    },
+    {
+      "id": "PLL Rb",
+      "name": "PLL Rb",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Rb"
+    },
+    {
+      "id": "PLL T",
+      "name": "PLL T",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/T"
+    },
+    {
+      "id": "PLL Ua",
+      "name": "PLL Ua",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Ua"
+    },
+    {
+      "id": "PLL Ub",
+      "name": "PLL Ub",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Ub"
+    },
+    {
+      "id": "PLL V",
+      "name": "PLL V",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/V"
+    },
+    {
+      "id": "PLL Y",
+      "name": "PLL Y",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Y"
+    },
+    {
+      "id": "PLL Z",
+      "name": "PLL Z",
+      "group": "PLL",
+      "ref": "https://www.speedcubedb.com/a/3x3/PLL/Z"
+    }
+  ]
+};
