754e6f4a25d8514ab99b1eacaabb5a03a1f9e779
Davi-077 <davisouzasanchez@gmail.com>
2026-09-14 16:52:58 -0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// CubeTrain - configuração do login
-// Cole SOMENTE a Publishable Key do Clerk aqui.
-// Exemplo: pk_test_...
-// NÃO coloque Secret Key neste arquivo nem no GitHub.
-
-window.CUBETRAIN_CONFIG = {
-  CLERK_PUBLISHABLE_KEY: "COLE_SUA_PUBLISHABLE_KEY_AQUI"
-};
